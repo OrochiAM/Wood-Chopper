cffe3ac039a9db26990fe18f1eb1a155648b810e
Aleksa Milic <baba12milic@gmail.com>
2025-05-31 08:57:37 +0200

    Left right movement 2.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 const lumberjack = document.querySelector('.lumberjack');
-
+const windowWidth = window.innerWidth;
 window.addEventListener('keydown', (e) => {
-  if (e.key === 'ArrowLeft') {
+  let keyPressed = e.key;
+
+  if (keyPressed === 'ArrowLeft' || keyPressed === 'a') {
     lumberjack.className = 'lumberjack lj-left';
-  } else if (e.key === 'ArrowRight') {
+  } else if (keyPressed === 'ArrowRight' || keyPressed === 'd') {
+    lumberjack.className = 'lumberjack lj-right';
+  }
+});
+
+window.addEventListener('click', (e) => {
+  let clickPosition = e.clientX;
+
+  if (clickPosition < windowWidth / 2) {
+    lumberjack.className = 'lumberjack lj-left';
+  } else {
     lumberjack.className = 'lumberjack lj-right';
   }
 });
